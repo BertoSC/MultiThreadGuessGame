864ac1b545137ae02e46b1b8c4df3f9fa0fcb03b
bertoSC <a23albertogc@iessanclemente.net>
2025-01-30 14:34:44 +0100

    GUESSED V1 done

--- src/main/java/org/example/GuessClient.java
+++ src/main/java/org/example/GuessClient.java
@@ -1,4 +1,37 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class GuessClient {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 57780);
+            var flujoIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            var flujoOut = new PrintWriter(socket.getOutputStream(), true);
+            String inicioServer = flujoIn.readLine();
+            System.out.println(inicioServer);
+            while (true){
+                Scanner entrada = new Scanner(System.in);
+                System.out.println("Introduce el comando de petición: ");
+                String peticion = entrada.nextLine();
+                if (peticion.equalsIgnoreCase("QUIT")){
+                    flujoOut.println(peticion);
+                    String respuesta = flujoIn.readLine();
+                    System.out.println(respuesta);
+                    break;
+                }
+                flujoOut.println(peticion);
+                String respuesta = flujoIn.readLine();
+                System.out.println(respuesta);
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error de entrada y salida: "+ e.getMessage());
+        }
+    }
 }
--- src/main/java/org/example/GuessServerWorker.java
+++ src/main/java/org/example/GuessServerWorker.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 
 public class GuessServerWorker implements Runnable{
     private Socket socket;
@@ -14,15 +15,15 @@ public class GuessServerWorker implements Runnable{
     private BufferedReader in;
     private PrintWriter out;
 
-    public GuessServerWorker(Socket socket){
+    public GuessServerWorker(Socket socket) throws IOException {
         this.socket=socket;
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
     }
 
     @Override
     public void run() {
-        try (var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             var out = new PrintWriter(socket.getOutputStream(), true)){
-
+        try {
             out.println("10 Number game server ready");
             String comando;
 
@@ -31,12 +32,12 @@ public class GuessServerWorker implements Runnable{
             }
 
         } catch (IOException ex){
-
+            out.println("15 Number game server ready");
         }
 
     }
 
-    private void responderAccion(String comando) {
+    private void responderAccion(String comando) throws IOException {
         String[] argumentos = comando.split(" ");
         String peticion = argumentos[0];
 
@@ -52,19 +53,20 @@ public class GuessServerWorker implements Runnable{
                 break;
             case "QUIT":
                 out.println("11 BYE");
+                socket.close();
                 break;
             default:
                 out.println("90 UNKNOWN");
+                break;
 
         }
     }
 
     private void mostrarInformacion() {
-        out.println("40 INFO");
-        out.println("NEW. This command indicates that the client wants to start a new game. As an argument it accept the number of tries the user want to have to guess the number. Example: NEW 8");
-        out.println("NUM. The client sends its guess to the server. A new game has to be created before using this command. Example: NUM 42.");
-        out.println("HELP. The client asks the server for information about the game and the commands to use.");
-        out.println("QUIT. The client sends the request to terminate the communication with the server.");
+        out.println("40 INFO"+
+                "NEW. This command indicates that the client wants to start a new game. As an argument it accept the number of tries the user want to have to guess the number. Example: NEW 8"+
+                        "HELP. The client asks the server for information about the game and the commands to use."+
+                        "QUIT. The client sends the request to terminate the communication with the server.");
     }
 
     private void empezarPartida(String[] argumentos) {
@@ -73,16 +75,57 @@ public class GuessServerWorker implements Runnable{
             return;
         }
         try {
-            intentos = Integer.parseInt(argumentos[1]);
-            jugando = true;
+            this.intentos = Integer.parseInt(argumentos[1]);
+            this.jugando = true;
+            Random rand = new Random();
+            this.numSecreto = rand.nextInt(100) + 1;
             out.println("20 PLAY <" + intentos + ">");
         }catch (NumberFormatException num){
-            System.out.println("80 ERR");
+            out.println("80 ERR");
         }
 
     }
 
     private void gestionarAdivinanza(String[] argumentos) {
+
+        if (!jugando){
+            out.println("80 ERR");
+            return;
+        }
+
+        if (intentos==0){
+            out.println("70 LOSE NUM <"+numSecreto+">");
+            this.jugando=false;
+            return;
+        }
+
+        if (argumentos.length!=2){
+            out.println("90 UNKNOWN");
+            return;
+        }
+
+        int intento = Integer.parseInt(argumentos[1]);
+
+        if (intento<numSecreto){
+            intentos--;
+            out.println("25 LOW <"+intentos+">");
+            return;
+        }
+
+        if (intento>numSecreto){
+            intentos--;
+            out.println("35 HIGH <"+intentos+">");
+            return;
+        }
+
+        if (intento==numSecreto){
+            out.println("50 WIN");
+            this.jugando=false;
+            return;
+        }
+
+
+
     }
 
 
